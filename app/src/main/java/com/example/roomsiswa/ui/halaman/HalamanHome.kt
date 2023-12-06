1718559f7e44e62af6bc859f27b8b1c85523e005
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2023-12-06 20:55:29 +0700

    Tambah file HalamanHome.kt dan buat object DestinasiHome

---
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
@@ -0,0 +1,11 @@
+package com.example.roomsiswa.ui.halaman
+import com.example.roomsiswa.R
+import com.example.roomsiswa.data.Siswa
+import com.example.roomsiswa.navigasi.DestinasiNavigasi
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
+
+
