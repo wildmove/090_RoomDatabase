1fdc98fa92f4718e9fd66172c824c643d761b12c
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2023-12-06 19:49:56 +0700

    Tambah HomeViewModel.kt pada package model

---
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.roomsiswa.model
+
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.repositori.RepositoriSiswa
+
+class HomeViewModel (private val repositoriSiswa: RepositoriSiswa):ViewModel() {
+
+}
